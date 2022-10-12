302051b60c708e77432673ee788d112b63a87112
kuueli <ophelie.diomar@epitech.eu>
2022-10-12 15:36:10 +0200

    Add Suspense for Spline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
+import React, { useState, Suspense } from 'react';
 import './App.css';
 import axios from 'axios';
 import Search from './components/Search/search.jsx';
 import Spline from '@splinetool/react-spline';
 import logo from './assets/images/weathy-logo.png';
+import { ColorRing } from 'react-loader-spinner'
 
 function App() {
+  const [loading, setLoading] = useState(false)
   return (
     <div className="App">
       <header>
@@ -19,9 +22,19 @@ function App() {
       </header>
       <main>
         <Search />
-        <div className='spline'>
-          <Spline scene="https://prod.spline.design/z8ML-U1cYT2gsIdI/scene.splinecode" />
-        </div>
+        <Suspense fallback={<ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+          colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+        />}>
+          <div className='spline'>
+            <Spline scene="https://prod.spline.design/z8ML-U1cYT2gsIdI/scene.splinecode" />
+          </div>
+        </Suspense>
       </main>
     </div>
   );
