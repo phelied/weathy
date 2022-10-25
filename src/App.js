9cd4d808db62aa5b6c7bc72b9fe8e57e4cab01b9
kuueli <ophelie.diomar@epitech.eu>
2022-10-26 00:36:30 +0200

    Add geolocalisation choice

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import { ColorRing } from 'react-loader-spinner';
 // import SplineHook from './hooks/3D/spline';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faXmark, faWind } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faXmark, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons'
 import API from "./hooks/utils/API";
 
 // const SplineHook = React.lazy(() => import('./hooks/3D/spline'));
@@ -36,6 +37,38 @@ function App() {
     setWordEntered("");
   };
 
+  function getLocalisationUser() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      API.ApiWeather(position.coords.latitude, position.coords.longitude).then((data) => { setWeatherData(data) });
+    });
+  };
+
+  function errorsLocalisationUser(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  // get the localisation of the user
+  const askLocalisationUser = () => {
+    if (navigator.geolocation) {
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          if (result.state === "granted") {
+            navigator.geolocation.getCurrentPosition(getLocalisationUser);
+          } else if (result.state === "prompt") {
+            navigator.geolocation.getCurrentPosition(getLocalisationUser, errorsLocalisationUser);
+          } else if (result.state === "denied") {
+            //If denied then you have to show instructions to enable location
+          }
+          result.onchange = function () {
+            console.log(result.state);
+          };
+        });
+    } else {
+      alert("Sorry Not available!");
+    }
+  };
+
   useEffect(() => {
     if (wordEntered !== "") {
       const timer = setTimeout(() => {
@@ -46,21 +79,9 @@ function App() {
     }
   }, [wordEntered]);
 
-  useEffect(() => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        API.ApiWeather(position.coords.latitude, position.coords.longitude).then((data) => { setWeatherData(data) });
-      });
-    } else {
-      console.log("Not Available");
-    }
-  }, []);
-
   const handleToggle = () => {
     setActive(!isActive);
   };
-
-
 
   return (
     <div className="App">
@@ -82,7 +103,13 @@ function App() {
                 value={wordEntered} placeholder="Search for a city"
                 onChange={(e) => setWordEntered(e.target.value.trim())} /> </>)
             :
-            (<><button onClick={handleToggle} className='btn-search'><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+            (<>
+              <button onClick={askLocalisationUser} className='btn-search btn-localisation'>
+                <FontAwesomeIcon icon={faLocationDot} />
+              </button><button onClick={handleToggle} className='btn-search'>
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </button>
+
               <input type="text" className='input-search' value={wordEntered}
                 onChange={(e) => setWordEntered(e.target.value.trim())} /></>)}
         </div>
@@ -127,7 +154,7 @@ function App() {
             wrapperClass="blocks-wrapper"
             colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
           />}>
-            <iframe src='https://my.spline.design/molang3dcopy-a77b38a508355d692790536f2ad9fd1c/' frameBorder='0' width='100%' height='100%'></iframe>
+            <iframe title="3D Design" src='https://my.spline.design/molang3dcopy-a77b38a508355d692790536f2ad9fd1c/' frameBorder='0' width='100%' height='100%'></iframe>
           </Suspense>
         </div>
       </main>
@@ -151,8 +178,8 @@ function App() {
         {weatherData && weatherData.length !== 0 ? (
           weatherData.daily.slice(1, 5).map((day, index) => (
             <div className='aside__weather-item' key={index}>
-              <span className='aside__weather-item-day'>{new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(new Date(day.dt * 1000))}</span>
-              <img className='aside__weather-item-icon' src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`} />
+              <span className='aside__weather-item-day'>{new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(new Date(day.dt * 1000)).substring(0, 4)}.</span>
+              <img className='aside__weather-item-icon' alt='weather-icon' src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`} />
               <span className='aside__weather-item-temp'>{Math.round(day.temp.min)}°• {Math.round(day.temp.max)}°</span>
             </div>
           ))) :
@@ -184,8 +211,11 @@ function App() {
         }
       </aside>
       <footer>
-        <li className='navbar__link-item'><a href="https://fr.linkedin.com/in/ophelie-diomar-680162209" target="_blank" rel="noreferrer">LinkedIn</a></li>
-        <li className='navbar__link-item'><a href="https://github.com/phelied/weathy" target="_blank" rel="noreferrer">Github</a></li>
+        <span className='navbar__link-item'>
+          <a href="https://github.com/phelied/weathy" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faSquareGithub} />
+          </a>
+        </span>
       </footer>
     </div>
   );
