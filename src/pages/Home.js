d5a2c254b2affc33c5a8b9bb806be7295f3bd58f
kuueli <ophelie.diomar@epitech.eu>
2023-03-16 21:43:58 +0000

    Change tailwindCSS to styled-components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,7 @@ import Chart from 'chart.js/auto';
 import clouds from "../assets/images/clouds.png";
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import API from "../utils/API";
+import styled from 'styled-components';
 
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -35,9 +36,6 @@ const Home = () => {
             },
         ],
     };
-
-
-
 
     useEffect(() => {
         API.ApiWeather(defaultCity.lat, defaultCity.lon).then((data) => {
@@ -73,58 +71,58 @@ const Home = () => {
     }
 
 
-    return (<div className="app px-4">
-        <header className="flex justify-between py-6 bg-white items-center">
+    return (<AppContainer>
+        <Header>
             <Search getWeatherData={getWeatherData} />
-        </header>
-        {error && <div className="alert alert-danger">{error}</div>}
+        </Header>
+        {error && <AlertDanger>{error}</AlertDanger>}
         {weatherData && weatherData.length !== 0 &&
-            <main className='bg-white text-[#1A2840]'>
-                <div className='font-["Jost"] flex h-max flex-col bg-[#BEE6E6] rounded-xl back'>
-                    <div className='flex pl-2 pt-4 items-center '>
-                        <FontAwesomeIcon className='h-4 bg-white py-2 px-1 rounded-full text-[#FDAA67]' icon={faCloudSun} />
+            <Main>
+                <WeatherCard theme={{ backgroundColor: "#BEE6E6" }}>
+                    <WeatherCardContent>
+                        <WeatherIcon icon={faCloudSun} />
                         <div className='flex pl-2 flex-col'>
                             <span className='text-xl font-semibold'>{weatherData.name}</span>
                             <span className='text-sm'>What's the weather ? </span>
                         </div>
-                    </div>
-                    <div className='flex flex-col pl-2 pt-4 mt-2'>
-                        <div className='flex pl-2 flex-row items-center'>
-                            <span className='text-3xl font-semibold'>{Math.floor(weatherData.main.temp)}°C</span>
-                            <span className='text-sm bg-white ml-4 rounded-md px-2 py-px font-semibold'>{Math.floor(weatherData.main.temp_max)}°C</span>
-                        </div>
-                        <span className='h-5 pl-2 text-black text-sm'>{weatherData.weather[0].description}</span>
-                    </div>
-                    <div className='grid grid-cols-3 gap-x-2 mx-3 my-6 justify-around'>
-                        <div className='flex flex-col items-center py-2 justify-center bg-[#1A2840] text-white rounded-xl'>
-                            <span className='text-sm font-medium'>Pressure</span>
-                            <span className='text-base font-semibold'>{weatherData.main.pressure} mb</span>
-                        </div>
-                        <div className='flex flex-col  items-center py-2 justify-center bg-[#CBE175] rounded-xl'>
-                            <span className='text-sm font-medium'>Visibility</span>
-                            <span className='text-base font-semibold'>{weatherData.visibility / 1000}km</span>
-                        </div>
-                        <div className='flex flex-col items-center py-2 justify-center bg-white rounded-xl'>
-                            <span className='text-sm font-medium'>Humidity</span>
-                            <span className='text-base font-semibold'>{weatherData.main.humidity}%</span>
-                        </div>
-                    </div>
-                </div>
-                <div className='font-["Jost"] flex h-max flex-col bg-[#55ADE2] rounded-xl mt-6 text-white'>
-                    <div className='flex pl-2 pt-4 items-center '>
-                        <FontAwesomeIcon className='h-4 bg-white py-2 px-2 rounded-full text-[#FDAA67]' icon={faWind} />
+                    </WeatherCardContent>
+                    <WeatherTempContainer>
+                        <WeatherTemp>
+                            <h4>{Math.floor(weatherData.main.temp)}°C</h4>
+                            <span>{Math.floor(weatherData.main.temp_max)}°C</span>
+                        </WeatherTemp>
+                        <WeatherDescription>{weatherData.weather[0].description}</WeatherDescription>
+                    </WeatherTempContainer>
+                    <WeatherStats>
+                        <WeatherStatCard theme={{ color: "white", backgroundColor: "#1A2840" }}>
+                            <h6>Pressure</h6>
+                            <h3>{weatherData.main.pressure} mb</h3>
+                        </WeatherStatCard>
+                        <WeatherStatCard theme={{ backgroundColor: "#CBE175" }}>
+                            <h6>Visibility</h6>
+                            <h3>{weatherData.visibility / 1000}km</h3>
+                        </WeatherStatCard>
+                        <WeatherStatCard theme={{ backgroundColor: "white" }}>
+                            <h6>Humidity</h6>
+                            <h3>{weatherData.main.humidity}%</h3>
+                        </WeatherStatCard>
+                    </WeatherStats>
+                </WeatherCard>
+                <WeatherCard theme={{ backgroundColor: "#55ADE2", color: "white" }}>
+                    <WeatherCardContent>
+                        <WeatherIcon icon={faWind} />
                         <div className='flex pl-2 flex-col'>
                             <span className='text-xl font-semibold'>Air Quality</span>
                             <span className='text-sm'>What's the weather ? </span>
                         </div>
-                    </div>
-                    <div className='flex flex-col pl-2 pt-4 mt-2'>
+                    </WeatherCardContent>
+                    <WeatherTempContainer>
                         <div className='flex pl-2 flex-row items-center'>
                             <span className='text-3xl font-semibold'>{calculateAqi(weatherData.air.components["pm2_5"])}</span>
                             <span className='text-sm bg-white ml-4 rounded-md font-semibold px-2 bg-[#CBE175] py-1 text-[#1A2840]'>AQI</span>
                         </div>
                         <span className='h-5 pl-2 text-white text-sm'>West Wind</span>
-                    </div>
+                    </WeatherTempContainer>
                     <div className='mt-10 mx-2 rounded-xl bg-white text-[#1A2840] px-3 py-3 mb-3'>
                         <div>
                             <span className='text-sm font-semibold'>Good</span>
@@ -132,7 +130,7 @@ const Home = () => {
                         </div>
                         <ProgressBar className='mt-2 h-2 text-black' variant="warning" animated now={weatherData.air.main.aqi * 20} />
                     </div>
-                </div>
+                </WeatherCard>
                 {/* <div className='text-[#1A2840] font-bold text-3xl mt-8'>
                     <span>How's the</span><br />
                     <span>temperature today ? </span>
@@ -218,7 +216,7 @@ const Home = () => {
                         <span className='h-5 mt-2 text-black text-base'>{weatherData.forecast.weather[0].description}</span>
                     </div>
                 </div>
-            </main>
+            </Main>
         }
         <footer>
             <nav className='nav-footer__link mt-2 flex justify-end'>
@@ -227,7 +225,131 @@ const Home = () => {
                 </a>
             </nav>
         </footer>
-    </div>);
+    </AppContainer>);
 };
+
+const AppContainer = styled.div`
+  padding: 1rem;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+  background-color: white;
+`;
+
+const AlertDanger = styled.div`
+  background-color: #f8d7da;
+  border-color: #f5c6cb;
+  color: #721c24;
+  padding: .75rem 1.25rem;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+  margin: 1rem 0;
+`;
+
+const Main = styled.main`
+  background-color: white;
+  color: #1A2840;
+`;
+
+const WeatherCard = styled.div`
+  font-family: 'Jost', sans-serif;
+  display: flex;
+  flex-direction: column;
+  height: max-content;
+  border-radius: 1rem;
+  margin-top: 1rem;
+  color: ${props => props.theme.color};
+  background-color: ${props => props.theme.backgroundColor};
+`;
+
+const WeatherCardContent = styled.div`
+  display: flex;
+  padding-left: 1rem;
+  padding-top: 1rem;
+  align-items: center;
+`;
+
+const WeatherIcon = styled(FontAwesomeIcon)`
+  height: 1rem;
+  background-color: white;
+  padding: 0.5rem 0.50rem;
+  border-radius: 9999px;
+  color: #FDAA67;
+`;
+
+const WeatherTempContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 0.5rem;
+  padding-top: 1.0rem;
+  margin-top: 0.5rem;
+`;
+
+const WeatherTemp = styled.div`
+    display: flex;
+    padding-left: 0.5rem;
+    flex-direction: row;
+    align-items: center;
+    font-weight: 600;
+
+  & span {
+    background-color: white;
+    font-size: 0.875rem;
+    margin-left: 1rem;
+    padding: 1px 0.5rem;
+    border-radius: 0.375rem;
+    font-weight: 600;
+  }
+
+  & h4 {
+    font-size: 1.875rem;
+    line-height: 2.25rem;
+    font-weight: 600;
+  }
+`;
+
+const WeatherDescription = styled.span`
+  height: 1.25rem;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  padding-left: 0.5rem;
+  color: black;
+`;
+
+const WeatherStats = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 0.5rem;
+  justify-content: space-around;
+  margin: 1.5rem 0.75rem;
+`;
+
+const WeatherStatCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem 0;
+  border-radius: 0.75rem;
+
+  background-color: ${props => props.theme.backgroundColor};
+  color: ${props => props.theme.color};
+
+  & h6 {
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    font-weight: 500;
+  }
+
+  & h3 {
+    font-size: 1rem;
+    line-height: 1.5rem;
+    font-weight: 600;
+  }
+`;
 
 export default Home;
